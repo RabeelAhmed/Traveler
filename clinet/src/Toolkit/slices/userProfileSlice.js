0ee1a1b4c5eb8bd0ebf65b5e5f40a24c61c6578a
Rabeel Ahmed <rabeelsulehria3@gmail.com>
2026-07-01 05:52:16 -0700

    feat(client): configure global store and main config slices

---
+++ clinet/src/Toolkit/slices/userProfileSlice.js
@@ -0,0 +1,103 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { axiosClient } from "../../utils/axiosClient"; // Assuming you have this axios client set up.
+import toast from "react-hot-toast";
+
+export const getUserProfile = createAsyncThunk(
+  "user/getUserProfile",
+  async (userId, { rejectWithValue }) => {
+    try {
+      // Make sure you are sending the correct API call and parameters
+      const response = await axiosClient.get(`/user/getUserProfile/${userId}`);
+      console.log(response);
+      return response.data; // Adjust based on your response structure
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  }
+);
+export const followAndUnfollowUser = createAsyncThunk(
+  "user/followAndUnfollow",
+  async (body) => {
+    try {
+      const response = await axiosClient.post("/user/follow", body);
+      console.log(response.data.result);
+      toast.success(response.data.result.message);
+      return response.data.result;
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+);
+
+const userProfileSlice = createSlice({
+  name: "userProfile",
+  initialState: {
+    user: {},
+    isFollowing: false,
+    posts: [],
+    loading: false,
+    error: null,
+    message:"",
+  },
+  reducers: {
+    addPost: (state,action) =>{
+      
+    },
+    resetProfile(state) {
+      state.user = null;
+      state.posts = [];
+      state.isFollowing = false;
+      state.loading = false;
+    },
+    toggleLike: (state, action) => {
+      const postId = action.payload.postId;
+      const curUserId = action.payload.curUserId;
+      console.log(postId,curUserId);
+      const post = state.posts.find((post) => post.id === postId);
+      if (post) {
+        const isLiked = post.likes.includes(curUserId);
+        if (isLiked) {
+          post.likesCount -= 1;
+          post.isLikedByUser = false;
+        } else {
+          post.likesCount += 1;
+          post.isLikedByUser = true;
+        }
+      }
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getUserProfile.pending, (state) => {
+        state.loading = true;
+        state.error = null; // Clear any previous error
+      })
+      .addCase(getUserProfile.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action?.payload?.data?.userProfile; // Store user profile data
+        state.posts = action?.payload?.data?.posts;
+        state.message = action?.payload?.message;
+        state.isFollowing = action?.payload?.data?.isFollowing;
+      })
+      .addCase(getUserProfile.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload; // Store error message if request fails
+      })
+      .addCase(followAndUnfollowUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(followAndUnfollowUser.rejected, (state, action) => {
+        state.status = "failed"; // Handle failed state
+        state.error = action.payload;
+      })
+      .addCase(followAndUnfollowUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user.followers = action.payload.user.followersCount;
+        state.user.following = action.payload.user.followingCount;
+        state.isFollowing = action.payload.user.isFollowing;
+      });
+  },
+});
+export const { toggleLike,resetProfile } = userProfileSlice.actions;
+export default userProfileSlice.reducer;
---
+++ clinet/src/Toolkit/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import appConfigReducer from "./slices/appConfigSlice"
+import feedReducer from "./slices/feedSlice";
+import storyReducer from "./slices/storySlice";
+import userProfileReducer from "./slices/userProfileSlice";
+import journeyReducer from "./slices/journeySlice";
+export default configureStore ({
+    reducer: {
+        appConfig: appConfigReducer,
+        feed: feedReducer,
+        userProfile: userProfileReducer,
+        story:storyReducer,
+        journey: journeyReducer,
+    }
+})
