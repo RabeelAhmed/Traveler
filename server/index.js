aa765c368743dd9d94f242d7c180ddbcefff0a10
Rabeel Ahmed <rabeelsulehria3@gmail.com>
2026-07-01 05:52:16 -0700

    feat(server): set up WebSocket notification emitter

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const dbconnection = require('./db');
+const http = require('http'); // Required for socket.io
+const { Server } = require('socket.io');
 const authentication = require('./Routers/authenticationRouters');
-const postRouter = require('./Routers/postRouter');
-const story = require('./Routers/storyRouter');
-const journeyRouter = require('./Routers/journeyRouter');
-const userRouter = require('./Routers/userRouter');
 const morgan = require('morgan');
 const cors = require('cors');
 
@@ -13,25 +11,38 @@ dbconnection;
 
 const app = express()
 const port = process.env.PORT || 3000;
+const origin_env = process.env.ORIGIN
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: origin_env, // Frontend URL
+    credentials: true,
+  },
+});
+
+const story = require('./Routers/storyRouter');
+const postRouter = require('./Routers/postRouter');
+const userRouter = require('./Routers/userRouter');
+const journeyRouter = require('./Routers/journeyRouter');
+const {initsocket} = require('./socket')
+initsocket(io);
 
 app.use(cors({ 
   credentials: true,
-  origin: process.env.ORIGIN,
+  origin:origin_env,
 }));
 app.use(morgan('common'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-app.use('/auth', authentication)
-app.use('/post', postRouter)
-app.use('/story', story)
-app.use('/journey', journeyRouter)
-app.use('/user', userRouter)
-
+app.use('/auth',authentication)
+app.use('/story',story)
+app.use('/post',postRouter)
+app.use('/user',userRouter)
+app.use('/journey',journeyRouter)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
