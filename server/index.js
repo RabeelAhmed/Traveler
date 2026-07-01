f8f663d87c2531c3103889b8c08f68272bf7b087
Rabeel Ahmed <rabeelsulehria3@gmail.com>
2026-07-01 05:52:07 -0700

    feat(server): establish MongoDB connection client

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const dbconnection = require('./db');
 const morgan = require('morgan');
 const cors = require('cors');
 
 require('dotenv').config()
+dbconnection;
 
 const app = express()
 const port = process.env.PORT || 3000;
