1b9b46efc0c789175937028caaa2ad2ff0a7f43c
Rabeel Ahmed <rabeelsulehria3@gmail.com>
2026-07-01 05:52:09 -0700

    feat(server): implement like and comment interactions on posts

--- server/Controllers/postController.js
+++ server/Controllers/postController.js
@@ -135,8 +135,209 @@ const getPost = async (req, res) => {
     return res.status(500).send(error("Something went wrong"));
   }
 };
+const likeAndUnlikePost = async (req, res) => {
+  try {
+    const { postId } = req.body;
+    const curUserId = req.user.user_Id;
+    // Find the post by ID and populate the userId reference
+    const post = await Post.findById(postId).populate("userId");
+    const postOwner = await user.findById(post.userId);
+    if (!post) {
+      return res.status(404).json(error(404, "Post Not Found"));
+    }
+
+    // Check if the user has already liked the post
+    const isLiked = post.likes.includes(curUserId);
+
+    // Perform the atomic like/unlike operation
+    const updateOperation = isLiked
+      ? { $pull: { likes: curUserId } } // Remove the user ID from likes array
+      : { $addToSet: { likes: curUserId } }; // Add the user ID to likes array
+    // Update the post and return the updated post
+    let achivement;
+    if (post.likes.length === 0 && !isLiked) {
+      achivement = "explorer";
+
+      const hasBadge = postOwner.badges.some((obj) => obj.name === achivement);
+
+      if (!hasBadge) {
+        postOwner.badges.push({
+          name: achivement,
+          awardedAt: new Date(),
+        });
+
+        await postOwner.save();
+        const notification = new Notification({
+          recipient: post.userId._id, // Post owner
+          sender: post.userId._id,
+          type: "Achivement",
+          post: postId,
+        });
+        await notification.save();
+        notify(notification);
+      }
+    }
+
+    const message = isLiked
+      ? "You have unliked the post."
+      : "You have liked the post.";
+    const updatedPost = await Post.findByIdAndUpdate(postId, updateOperation, {
+      new: true,
+    }).populate("userId");
+    console.log(updatedPost);
+    const responsePost = await updatedPost.populate({
+      path: "comments", // Populate comments
+      populate: {
+        path: "userId", // Assuming each comment has an 'author' field to populate
+        select: "fullname profilePicture",
+      },
+    });
+
+    console.log("post user", post.userId._id, "Post Id", postId);
+    if (!isLiked) {
+      if (!(post.userId._id.toString() === curUserId)) {
+        const notification = new Notification({
+          recipient: post.userId._id, // Post owner
+          sender: curUserId,
+          type: "like",
+          post: postId,
+        });
+        await notification.save();
+        notify(notification);
+      }
+    }
+    return res
+      .status(200)
+      .json(
+        success(200, { post: mapPostOutput(responsePost, curUserId), message })
+      );
+  } catch (err) {
+    console.error("Error in likeAndUnlikePost:", err); // Log the error for debugging purposes
+    return res.status(500).json(error(500, "Something went wrong"));
+  }
+};
+const addComment = async (req, res) => {
+  try {
+    const { postId, commentText } = req.body;
+    const curUserId = req.user.user_Id;
+
+    // Find the post by ID and populate the userId reference
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json(error(404, "Post Not Found"));
+    }
+    const postOwner = await user.findById(post.userId);
+    // Create a comment object
+    const comment = {
+      userId: curUserId,
+      commentText: commentText,
+    };
+
+    let achivement;
+    if (post.comments.length === 0) {
+      achivement = "Nature_Lover";
+      console.log("In COmment");
+      const hasBadge = postOwner.badges.some((obj) => obj.name === achivement);
+
+      if (!hasBadge) {
+        postOwner.badges.push({
+          name: achivement,
+          awardedAt: new Date(),
+        });
+
+        await postOwner.save();
+        const notification = new Notification({
+          recipient: post.userId._id, // Post owner
+          sender: post.userId._id,
+          type: "Achivement",
+          post: postId,
+        });
+        await notification.save();
+        notify(notification);
+      }
+    }
+
+    // Push the comment object into the comments array
+    post.comments.push(comment);
+
+    // Save the updated post
+    await post.save();
+    let responsePost = await Post.findById(postId)
+      .populate({
+        path: "userId", // Populate user data
+      })
+      .populate({
+        path: "comments", // Populate comments
+        populate: {
+          path: "userId", // Assuming each comment has an 'author' field to populate
+          select: "fullname profilePicture",
+        },
+      });
+    responsePost = mapPostOutput(responsePost, curUserId);
+    responsePost.comments = responsePost.comments.reverse();
+    console.log(responsePost.comments);
+
+    if (!(post.userId._id.toString() === curUserId)) {
+      const notification = new Notification({
+        recipient: post.userId._id, // Post owner
+        sender: curUserId,
+        type: "comment",
+        post: postId,
+      });
+      console.log("Inside Notify");
+      await notification.save();
+      console.log("Entring Notify");
+      notify(notification);
+    }
+    // Return the response with the mapped output of the updated post
+    return res.status(200).json(success(200, { responsePost }));
+  } catch (err) {
+    console.error("Error in addComment:", err); // Log the error for debugging purposes
+    return res.send(error(500, "Something went wrong"));
+  }
+};
+const deleteComment = async (req, res) => {
+  try {
+    const { postId, commentId } = req.body;
+    const curUserId = req.user.user_Id;
+
+    // Find the post by ID and populate comments with userId reference
+    const post = await Post.findById(postId).populate("comments.userId");
+    if (!post) {
+      return res.status(404).json(error(404, "Post Not Found"));
+    }
+
+    // Find the comment by ID
+    const comment = post.comments.id(commentId);
+    if (!comment) {
+      return res.status(404).json(error(404, "Comment Not Found"));
+    }
+
+    // Check if the comment belongs to the current user
+    if (comment.userId._id.toString() === curUserId) {
+      // Remove the comment from the comments array
+      post.comments.pull(commentId);
+      // Save the updated post
+      await post.save();
+
+      // Return the response with the mapped output of the updated post
+      return res
+        .status(200)
+        .json(success(200, { post: mapPostOutput(post, curUserId) }));
+    } else {
+      return res
+        .status(403)
+        .json(error(403, "Unauthorized to delete this comment"));
+    }
+  } catch (err) {
+    return res.send(error(500, "Something went wrong"));
+  }
+};
 module.exports = {
   createPost,
   getPost,
   generateSignature,
+  likeAndUnlikePost,
+  addComment,
+  deleteComment,
 };
--- server/Routers/postRouter.js
+++ server/Routers/postRouter.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
-const {createPost,getPost, generateSignature} = require('../Controllers/postController')
+const {createPost,likeAndUnlikePost,addComment,deleteComment,getPost, generateSignature} = require('../Controllers/postController')
 const {verifyAuthToken, optionalAuthToken} = require('../Middleware/jwtAuthMiddleware')
 const { multipleUpload } = require('../Middleware/uploads');
 
 router.post('/createpost',verifyAuthToken,multipleUpload,createPost);
 router.get('/signature',verifyAuthToken,generateSignature);
+router.post('/likepost',verifyAuthToken,likeAndUnlikePost);
+router.post('/addcomment',verifyAuthToken,addComment);
+router.post('/deletecomment',verifyAuthToken,deleteComment);
 router.get('/:_id',optionalAuthToken,getPost)
 module.exports = router;
