ddc903ae168cdf307d6ba38d7eae0a00a6d5ec8e
Rabeel Ahmed <rabeelsulehria3@gmail.com>
2026-07-01 05:52:07 -0700

    feat(server): configure response wrapper and upload middlewares

---
+++ server/Middleware/uploads.js
@@ -0,0 +1,17 @@
+const multer  = require('multer')
+
+
+const storage = multer.memoryStorage()
+
+const upload = multer({ storage: storage ,
+    limits: {
+        // limits file size to 5 MB
+        fileSize: 1024 * 1024 * 5
+    }
+    // ,fileFilter: fileFilterConfig,
+});
+const singleUpload = upload.single('profilePicture'); // For single file uploads
+const multipleUpload = upload.array('media[]', 5);   // For multiple file uploads (max 10)
+const storyUpload = upload.single('story')
+// Export the upload middleware
+module.exports = { singleUpload, multipleUpload,storyUpload };
